f343403b06e025d209ff9e6639ebb331266f2d5f
cerdieuulysse125-byte <cerdieuulysse125@gmail.com>
2026-08-27 03:09:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,2 +1,3 @@
-import "./globals.css"
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#facc15" />import "./globals.css"
 export default function RootLayout({children}:{children:React.ReactNode}){return <html><body>{children}</body></html>}
