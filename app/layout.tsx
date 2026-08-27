821d5ea319d87328ad8ac1d1da70c64f9b779394
cerdieuulysse125-byte <cerdieuulysse125@gmail.com>
2026-08-27 02:08:14 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,2 @@
-import React from "react"
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ht">
-      <body style={{margin:0}}>{children}</body>
-    </html>
-  )
-}
+import "./globals.css"
+export default function RootLayout({children}:{children:React.ReactNode}){return <html><body>{children}</body></html>}
