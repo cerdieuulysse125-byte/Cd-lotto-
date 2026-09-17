190b37b580f608a1ff51580389261b54ce0fd2ef
cerdieuulysse125-byte <cerdieuulysse125@gmail.com>
2026-09-16 22:02:06 -0400

    Update sw-vendeur.js

--- sw-vendeur.js
+++ sw-vendeur.js
@@ -1,5 +1,5 @@
 const LOGOS={
- nyFoto: "/new_york_lottery_badge.webp",
- flFoto: "/florida_lottery_logo.webp",
- gaFoto: "/georgia_lottery_emblem.webp",
+ nyFoto: "/ny.png",
+ flFoto: "/fl.png",
+ gaFoto: "/ga.png",
 }
